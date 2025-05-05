690ca85a08508d999fcc6b9d509321b8b4fbc423
GeneralGarfield <149305104+GeneralGarfield@users.noreply.github.com>
2025-05-05 18:43:19 -0500

    5/5/2025
    This is a hello world script

---
+++ Hello World!.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+
+    // This here is my first program Made by Ricardo!
+
+    printf("hello this is my first C work!\n");
+    printf("Made By Ricardo Montoya");
+    return 0;
+}
